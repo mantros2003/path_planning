9efe7dd39cfba4d40bfae600f166ce127555e153
swapnil <swapnil@Swapnils-MacBook-9.local>
2026-04-27 16:48:43 +0530

    Added benchmarking code

--- plugins/rrt_x.cpp
+++ plugins/rrt_x.cpp
@@ -20,6 +20,30 @@ PLUGINLIB_EXPORT_CLASS(
 
 namespace custom_planner {
 
+/**
+ * Helper function to compute the path length of the generated path
+ */
+double computePathLength(
+    const std::vector<geometry_msgs::PoseStamped>& plan)
+{
+    if (plan.size() < 2)
+        return 0.0;
+
+    double total_length = 0.0;
+
+    for (std::size_t i = 1; i < plan.size(); ++i) {
+        double dx = plan[i].pose.position.x -
+                    plan[i-1].pose.position.x;
+
+        double dy = plan[i].pose.position.y -
+                    plan[i-1].pose.position.y;
+
+        total_length += std::hypot(dx, dy);
+    }
+
+    return total_length;
+}
+
 // Function to initialize and store the costmap info once.
 void RRTXPlanner::initialize (std::string name, costmap_2d::Costmap2DROS* costmap_ros) {
     if (!initialized_) {
@@ -243,7 +267,8 @@ bool RRTXPlanner::makePlan(
     plan.push_back(goal);
 
     planned_ = true;
-    ROS_INFO("[RRTXPlanner] Successfully extracted path with %zu waypoints.", plan.size());
+    double path_len = computePathLength(plan);
+    ROS_INFO("[RRTXPlanner] Successfully extracted path with %zu waypoints,\tPath length: %.3f m,\tPlanning time: %.3f ms", plan.size(), path_len, (ros::Time::now() - start_time).toSec() * 1000);
     
     return true;
 }
