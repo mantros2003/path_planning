5c678398e067e6e5d8fff58416c47cd34b7a2ab6
Swapnil <swapnil.prein@gmail.com>
2026-07-08 07:05:55 +0530

    minor bug

--- plugins/rrt_x.cpp
+++ plugins/rrt_x.cpp
@@ -343,7 +343,7 @@ bool RRTXPlanner::extractPath(
             plan.clear();
             for (int i = 0; i < 5; i++) {
                 const Node& curr_node = nodes_[current_idx];
-                ROS_ERROR("Node index: %lu\tparent: %lu\tg: %f\tlmc: %f\tdist_to_par: %f", current_idx, curr_node.par_idx, curr_node.g, curr_node.lmc, distance(current_idx, curr_node.par_idx));
+                ROS_ERROR("Node index: %lu\tparent: %lu\t pos: (%f, %f)\tg: %f\tlmc: %f\tdist_to_par: %f", current_idx, curr_node.par_idx, curr_node.x, curr_node.y, curr_node.g, curr_node.lmc, distance(current_idx, curr_node.par_idx));
                 current_idx = curr_node.par_idx;
             }
             return false;
